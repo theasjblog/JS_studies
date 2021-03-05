39757fd52f91ecd64a67273adfa384720a38fcce
theasjblog <tequilacanaria@hotmail.it>
2021-03-05 09:26:20 +0000

    restructuring lessons accordion in one document

--- 00_tutorial/00.js
+++ 00_tutorial/00.js
@@ -9,20 +9,40 @@ Functions
 
 // print from for loop
 myLogTitle('Print for loop')
+
+// print code in page
+fullCodeString(
+    printCode("for (var i = 0; i < 5; i++) {", 0) +
+    printCode('myLog("Iteration of i " + i)', 1) +
+    printCode("}", 0)
+)
+
+// execute
 for (var i = 0; i < 5; i++) {
     myLog("Iteration of i " + i)
 }
 
+
 // Objects
 myLogTitle('Objetcs')
-var nameP = 'Adrian'
-var ageP = 37
-var hasDrivingLicenceP = true
 
+fullCodeString(
+    printCode("var person = {", 0) +
+    printCode('name: "Adrian"', 1) +
+    printCode('age: 37,', 1) +
+    printCode('hasDrivingLicence: true,', 1) +
+    printCode('// nested object', 1) +
+    printCode('address: {', 1) +
+    printCode("firstLine: '123',", 2) +
+    printCode("postCode: 'H1L',", 2) +
+    printCode("country: 'UK'", 2) +
+    printCode("}", 1) +
+    printCode("}", 0)
+)
 var person = {
-    name: nameP,
-    age: ageP,
-    hasDrivingLicence: hasDrivingLicenceP,
+    name: "Adrian",
+    age: 37,
+    hasDrivingLicence: true,
     // nested object
     address: {
         firstLine: '123',
@@ -30,32 +50,69 @@ var person = {
         country: 'UK'
     }
 }
-// ways to display objects
+
+myLogTitle("Ways to display objects")
+fullCodeString(printCode('person'))
 myLog(person)
+
+fullCodeString(printCode('person.nameP'))
 myLog(person.nameP)
+
+fullCodeString(printCode('JSON.stringify(person)'))
 myLog(JSON.stringify(person))
-myLog(Object.values(person))
+
+fullCodeString(printCode('JSON.stringify(person.address)'))
 myLog(JSON.stringify(person.address))
+
+fullCodeString(printCode('Object.keys(person)'))
 myLog(Object.keys(person))
+
+fullCodeString(printCode('Object.values(person)'))
 myLog(Object.values(person))
 
 // arrays
 myLogTitle('Arrays')
+fullCodeString(printCode("var names = ['Adrian', 'Alex', 'Maria']"))
 var names = ['Adrian', 'Alex', 'Maria']
+
+fullCodeString(printCode("names"))
 myLog(names)
+
+fullCodeString(printCode("names[2]"))
 myLog(names[2])
+
+fullCodeString(printCode("names.lenghth"))
 myLog(names.length)
+
 // loop method 1
+fullCodeString(
+    printCode("for (var i of names) {", 0) +
+    printCode('myLog("Iteration of i " + i)', 1) +
+    printCode('}', 0)
+)
 for (var i of names) {
     myLog("Iteration of i " + i)
 }
 // loop method 2
+fullCodeString(
+    printCode('names.forEach(function (i, index) {', 0) +
+    printCode('myLog(index + ":" + i)', 1) +
+    printCode('})', 0)
+)
 names.forEach(function (i, index) {
     myLog(index + ":" + i)
 })
 
 // functions
 myLogTitle('Functions')
+fullCodeString(
+    printCode('function doAddition(x, y) {', 0) +
+    printCode('return (x + y)', 1) +
+    printCode('}', 0) +
+    printCode('var x = 5', 0) +
+    printCode('var y = 7', 0) +
+    printCode('myLog("Sum of " + x + " and " + y + " is: " + doAddition(5, 7))', 0)
+)
 function doAddition(x, y) {
     return (x + y)
 }
--- 00_tutorial/auxFunctions.js
+++ 00_tutorial/auxFunctions.js
@@ -8,3 +8,19 @@ function myLogTitle(stuffPrint) {
     // <br> makes a new line
     document.write("<br><br><b>" + stuffPrint + "</b><br>")
 }
+
+function spacers() {
+    document.write("<br>*************************************<br>")
+}
+
+function printCode(codeLine, spacers) {
+    for (var i = 1; i <= spacers; i++) {
+        codeLine = "&nbsp;&nbsp;" + codeLine
+    }
+    return codeLine + "<br>"
+}
+
+function fullCodeString(codeString) {
+    document.write("<code>" + codeString + "</code>")
+
+}
