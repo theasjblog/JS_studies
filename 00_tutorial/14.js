7e33e9192099a8b53ef478c7c363f43a79536009
theasjblog <tequilacanaria@hotmail.it>
2021-03-16 17:10:01 +0000

    enhanced objects

--- 00_tutorial/14.js
+++ 00_tutorial/14.js
@@ -3,5 +3,30 @@ import * as printHelper from './helpers.js'
 
 const container = document.getElementById("container14");
 
+const pricePropName = 'PRICE'
+const calculator = (name, price) => {
+    return {
+        // if the argument has the same name as the key, we can replace
+        // name:name
+        // with:
+        name,
+        // if we create a function, we can replace 
+        // add: function (n1, n2) {
+        //     return n1 + n2
+        // }
+        // with
+        add(n1, n2) {
+            return n1 + n2
+        },
+        // here we use the const pricePropName as an object propery name: PRICE
+        // but we convert it to lower case
+        [pricePropName.toLowerCase()]: price
+    }
+}
+
+
 // Example 1
-printHelper.addChild(container, `<code>person.toString():</code>${'hi'}`)
+const calc = calculator('Marco', 19.99)
+printHelper.addChild(container, `<code>calc.name:</code>${calc.name}` + '<br>')
+printHelper.addChild(container, `<code>calc.add(10,20):</code>${calc.add(10, 20)}` + '<br>')
+printHelper.addChild(container, `<code>calc.price:</code>${calc.price}` + '<br>')
---
+++ 00_tutorial/15.js
@@ -0,0 +1,8 @@
+// import module
+import * as printHelper from './helpers.js'
+
+const container = document.getElementById("container15");
+
+
+// Example 1
+//printHelper.addChild(container, `<code>calc.name:</code>${calc.name}` + '<br>')
