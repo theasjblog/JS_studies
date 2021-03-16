13e8a8a4fcbd7081d082e7b2996e13cc5ac4b5a2
theasjblog <tequilacanaria@hotmail.it>
2021-03-16 15:21:12 +0000

    this

--- 00_tutorial/13.js
+++ 00_tutorial/13.js
@@ -1,7 +1,52 @@
 // import module
 import * as printHelper from './helpers.js'
 
-var container = document.getElementById("container13");
+const container = document.getElementById("container13");
 
 // Example 1
-printHelper.addChild(container, `<code>hello(): </code>${'placeholder'}` + '<br>')
+const person = {
+    'name': 'Alex',
+    'cars': ['ferrari', 'fiat'],
+    toString: function () {
+        return `${this.name} has ${this.cars}` + '<br>'
+    }
+}
+
+res = person.toString()
+printHelper.addChild(container, `<code>person.toString():</code>${res}`)
+
+/*
+Alternatives to use this context inside a function,
+not working for some reason
+const person = {
+    'name': 'Alex',
+    'cars': ['ferrari', 'fiat'],
+    toString: function () {
+        //const that = this
+        return this.cars.forEach(function (car) {
+            `${that.name} has ${car}`
+        })
+    }
+}
+
+const person = {
+    'name': 'Alex',
+    'cars': ['ferrari', 'fiat'],
+    toString: function () {
+        //const that = this
+        return this.cars.forEach(function (car) {
+            `${this.name} has ${car}`
+        }.bind(this)())
+    }
+}
+
+const person = {
+    'name': 'Alex',
+    'cars': ['ferrari', 'fiat'],
+    toString: function () {
+        //const that = this
+        return this.cars.forEach(car => `${this.name} has ${car}`)
+    }
+}
+*/
+
---
+++ 00_tutorial/14.js
@@ -0,0 +1,7 @@
+// import module
+import * as printHelper from './helpers.js'
+
+const container = document.getElementById("container14");
+
+// Example 1
+printHelper.addChild(container, `<code>person.toString():</code>${'hi'}`)
